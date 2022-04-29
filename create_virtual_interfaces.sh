4e83c22b0a045346c145d96a668aaf1caf2f1619
Abhik Jain <abhik19100@iiitnr.edu.in>
2022-04-29 12:09:49 +0530

    rewrite script to create dummy net devs

--- create_virtual_interfaces.sh
+++ create_virtual_interfaces.sh
@@ -1,7 +1,18 @@
+if [ -z $1 ]; then
+	echo "USAGE: ./create_virtual_interfaces.sh <number of virtual interfaces>"
+	exit 1
+fi
+
+sudo modprobe dummy
+if [ $? -ne 0 ]; then
+	echo "unable to load module 'dummy'"
+	exit 1
+fi
+
 for i in $(seq 0 $1); do
-	sudo ip link add eth0$i type dummy;
-	sudo ip link set dev eth0$i mtu 65536;
-	sudo ip link set eth0$i up;
+	sudo ip link add eth0$i type dummy
+	sudo ip link set dev eth0$i mtu 65536
+	sudo ip link set eth0$i up
 done
 
 # sudo ip link add eth00 type dummy
